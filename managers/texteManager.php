405cbda4912dc91b1e90ba9265b4f03cd46b8c64
Nicolas Mc Clure <nicolas.mcclue@hotmail.ca>
2013-04-26 18:22:11 -0400

    -correction de bug

--- managers/texteManager.php
+++ managers/texteManager.php
@@ -26,21 +26,11 @@ function write_file($chemin,$users){
 }
 
 
-<<<<<<< HEAD
+
 function save_file($chemin, $contenu_du_fichier) {
   $fichier = fopen($chemin, "w");
   fwrite($fichier, $contenu_du_fichier);
   fclose($fichier);
 }
 
-
-=======
-function open_file($chemin){
-	$fichier = fopen( $chemin, "r");
-	$contenu_du_fichier =  fread($fichier, filesize($chemin));
-    $users= unserialize($contenu_du_fichier);  
-	fclose($fichier);
-	return($users);
-}
->>>>>>> f26edd95c91cd9a346c8e3fa326f64d1fb80c7fd
 ?>
