2e5b360088f7ac7d5c562f27991a8e5df21183be
Nicolas Mc Clure <nicolas.mcclue@hotmail.ca>
2013-04-26 19:02:37 -0400

    -Correction du fichier user.inc -Création temporaire de la page ajout_donnees_users.php

---
+++ ajout_donnees_users.php
@@ -0,0 +1,22 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'TP/'.'initialisation.inc');
+      require_once("managers/texteManager.php");
+       //AJOUT
+  // 1.Lecture de tous les users
+  $chemin="data/user.inc";
+
+  $users=read_file($chemin);
+  
+  
+  $favoris=array(
+							'prod01',
+              'prod03',
+              'prod04'					
+												);
+
+
+//ecriture dans le tableau
+  $users['jv6dmc']['favoris']=$favoris;
+  write_file($chemin,$users);
+  
+  echo("modifications réussis!");
+?>
